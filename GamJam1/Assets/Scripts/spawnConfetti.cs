c8a3d9051c0644f119e8192dbfd05ab5dd43cbad
jyhrowolf <60275277+cetancopeland@users.noreply.github.com>
2020-09-17 21:23:17 -0700

    Confetti button and Planet Proof of Concept
    Created a mock planet proof of concept

--- GamJam1/Assets/Scripts/spawnConfetti.cs
+++ GamJam1/Assets/Scripts/spawnConfetti.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class spawnConfetti : MonoBehaviour
 {
     public GameObject ConeConfetti;
+    public GameObject Player;
+    private Transform pos;
+    public void Start()
+    {
+        pos = Player.GetComponent<Transform>();
+    }
     public void Click()
     {
-        GameObject ob = Instantiate(ConeConfetti);
+        GameObject ob = Instantiate(ConeConfetti,pos.position,Quaternion.LookRotation(transform.up,-transform.forward));
         Destroy(ob,3.0f);
     }
 }
