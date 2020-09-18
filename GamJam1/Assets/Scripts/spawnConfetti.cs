c1619cc7d63e7c03d3d934b7155da9f4c3591470
jyhrowolf <60275277+cetancopeland@users.noreply.github.com>
2020-09-17 18:32:30 -0700

    Confetti
    I added Confetti

---
+++ GamJam1/Assets/Scripts/spawnConfetti.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnConfetti : MonoBehaviour
+{
+    public GameObject ConeConfetti;
+    public void Click()
+    {
+        GameObject ob = Instantiate(ConeConfetti);
+        Destroy(ob,3.0f);
+    }
+}
