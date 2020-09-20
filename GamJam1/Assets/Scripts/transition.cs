ff0a664e1f2fd868fa3157ec084ad7cd5480bf05
jyhrowolf <60275277+cetancopeland@users.noreply.github.com>
2020-09-20 15:03:43 -0700

    Transition

--- GamJam1/Assets/Scripts/transition.cs
+++ GamJam1/Assets/Scripts/transition.cs
@@ -23,8 +23,10 @@ public class transition : MonoBehaviour
     }
     void Transition()
     {
-        oldLevel.transform.position = transform.up*50;
-        player.transform.position = transform.up*50;
+        oldLevel.transform.position = new Vector3(0f,50f,0f);
+        player.transform.position = new Vector3(player.transform.position.x,player.transform.position.y+50f,player.transform.position.z);
         Instantiate(newLevel);
+        collided = true;
+        Destroy(oldLevel,4.0f);
     }
 }
