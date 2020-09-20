9729723947ea3f8f38baf2ff8f3c7804385933b2
jyhrowolf <60275277+cetancopeland@users.noreply.github.com>
2020-09-19 22:48:08 -0700

    RisingWalls
    The walls rise.

---
+++ GamJam1/Assets/Scripts/wallRise.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wallRise : MonoBehaviour
+{
+    public GameObject player;
+    public float detectDistance;
+    public float riseSpeed;
+    private Transform pos;
+    private float distanceRisen = 0;
+    private float dist;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = player.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(pos.position,transform.position) <= detectDistance)
+            Rise();
+        else if(distanceRisen > 0)
+            Fall();
+    }
+    void Rise()
+    {
+        transform.position += transform.up*Time.deltaTime*riseSpeed;
+        distanceRisen += Time.deltaTime*riseSpeed;
+    }
+    void Fall()
+    {
+        transform.position -= transform.up*Time.deltaTime*riseSpeed;
+        distanceRisen -= Time.deltaTime*riseSpeed;
+    }
+}
