bc6d10075273ce01cf39960d8b489a48441af83d
jyhrowolf <60275277+cetancopeland@users.noreply.github.com>
2020-09-20 15:50:09 -0700

    AddColliders

---
+++ GamJam1/Assets/Scripts/AddColliders.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddColliders : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        AddCollider(transform);
+    }
+    void AddCollider(Transform trans)
+    {
+        foreach (Transform child in trans)
+        {
+            child.gameObject.AddComponent<MeshCollider>();
+            if(child.childCount>0)
+                AddCollider(child);
+        }
+    }
+}
