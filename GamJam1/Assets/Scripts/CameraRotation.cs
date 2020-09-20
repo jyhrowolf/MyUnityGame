f817a426b8972b756cb989e8cf9f35505a735ab8
jyhrowolf <60275277+cetancopeland@users.noreply.github.com>
2020-09-20 14:16:40 -0700

    forest take 1
    yes

---
+++ GamJam1/Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+
+    public GameObject player;
+    public float speed = 1;
+    private Vector3 old;
+    // Use this for initialization
+    void Start()
+    {
+        old = player.transform.position;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(player.transform.position, Vector3.down, speed * Time.deltaTime);
+        transform.position += player.transform.position-old;
+        old = player.transform.position;
+    }
+}
---
+++ GamJam1/Assets/Scripts/PlatformTilt.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class PlatformTilt : MonoBehaviour
+{
+    public float MaxTilt = 20.0f;
+    public float TSpeed = 250.0f;
+    public Vector3 CRotation;
+    float Xmax;
+    float Xmin;
+    float Zmax;
+    float Zmin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Initial Rotation of the platform
+
+        CRotation = this.transform.eulerAngles;
+
+        // Rotation Limits that do not exceed 20 degress in any rotation
+
+        Xmax = CRotation.x + MaxTilt;
+        Xmin = CRotation.x - MaxTilt;
+        Zmax = CRotation.z + MaxTilt;
+        Zmin = CRotation.z - MaxTilt;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotation updates for "w" and "s" keystrokes (Controls already worked out in a 
+        // way that made sense to rotate the platform).
+
+        CRotation.x += Input.GetAxis("Vertical") * Time.deltaTime * TSpeed;
+
+        // Rotation updates for "a" and "d" keystrokes (TSpeed is made negative in order to
+        // inverse the controls to make "a" shift the platform left and for "d" to shift
+        // the platform right instead of vice versa.
+
+        CRotation.z += Input.GetAxis("Horizontal") * Time.deltaTime * -TSpeed;
+
+        CRotation.x = Mathf.Clamp(CRotation.x, Xmin, Xmax);
+        CRotation.z = Mathf.Clamp(CRotation.z, Zmin, Zmax);
+
+        // Updates Currrent Angle
+
+        this.transform.eulerAngles = CRotation;
+    }
+}
