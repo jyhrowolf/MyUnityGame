3788eb0e375f6d51f8aa36e1835c394c2d26ce44
jyhrowolf <60275277+cetancopeland@users.noreply.github.com>
2020-09-20 15:29:42 -0700

    god help us

--- GamJam1/Assets/Scripts/CameraRotation.cs
+++ GamJam1/Assets/Scripts/CameraRotation.cs
@@ -6,18 +6,21 @@ public class CameraRotation : MonoBehaviour
 {
 
     public GameObject player;
-    public float speed = 1;
-    private Vector3 old;
+    private Rigidbody playerBody;
     // Use this for initialization
     void Start()
     {
-        old = player.transform.position;
+        playerBody = player.GetComponent<Rigidbody>();
     }
     // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(player.transform.position, Vector3.down, speed * Time.deltaTime);
-        transform.position += player.transform.position-old;
-        old = player.transform.position;
+        Vector2 offset = new Vector2(playerBody.velocity.x, playerBody.velocity.z);
+        offset.Normalize();
+        offset = offset * -1;
+        
+        transform.position = player.transform.position + new Vector3(offset.x, 0, offset.y);
+        transform.position = new Vector3(transform.position.x, player.transform.position.y+1.5f, transform.position.z);
+        transform.LookAt(new Vector3(player.transform.position.x, player.transform.position.y+0.5f, player.transform.position.z));
     }
 }
